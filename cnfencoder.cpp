775af5bde87f50ff12ab6a84c88f47522ff46c22
doyle-ngc <32763101+doyle-ngc@users.noreply.github.com>
2017-12-15 21:14:49 -0800

    Add files via upload

---
+++ cnfencoder.cpp
@@ -0,0 +1,166 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <fstream>
+#include <unistd.h>
+
+struct Node{
+  int id,x,y,k;
+};
+
+std::vector<Node> node_list;
+Node node;
+std::vector<std::string> cnf;
+
+int getNodeNo(int x,int y,int k){
+  for(auto e:node_list){
+    if(e.x==x && e.y==y && e.k==k){
+      return e.id;
+    }
+  }
+
+  return -1;
+}
+
+void addCNF0va(){
+  std::string str;
+  str += ("0");
+  cnf.push_back(str);
+}
+
+void addCNF1va(int cons){
+  std::string str;
+  str += std::to_string(cons);
+  cnf.push_back(str);
+}
+
+void addCNF2va(int cons1,int cons2){
+  std::string str;
+  str += ("-");
+  str += std::to_string(cons1);
+  str += (" ");
+  str += ("-");
+  str += std::to_string(cons2);
+  str += (" ");
+  str += ("0");
+  cnf.push_back(str);
+}
+
+void addCNF3va(int cons){
+  std::string str;
+  str += ("-");
+  str += std::to_string(cons);
+  cnf.push_back(str);
+}
+
+void addCNF4va(int k){
+  std::string str;
+  str += ("p");
+  str += (" ");
+  str += ("cnf");
+  str += (" ");
+  str += std::to_string(k);
+}
+
+
+void cnfencoder(int x,int y,int k,int adjacency[][200]){
+
+  //int x,y,k;
+  //int adjacency[k][k];
+  int count=1;
+  std::ofstream outputfile("input.txt");
+  std::vector<int> cons;
+  //adjacency[1][2] = 1;
+  //adjacency[2][3] = 1;
+  
+  
+  for(int i=0;i<k;i++){
+    for(int j=0;j<y;j++){
+      for(int l=0;l<x;l++){ 
+        node.id=count;
+        node.x=l;
+        node.y=j;
+        node.k=i;
+        node_list.push_back(node);
+        count++;
+      }
+    }
+  }
+
+  for(int i=0;i<k;i++){
+    for(int j=0;j<y;j++){
+      for(int l=0;l<x;l++){
+        addCNF1va(getNodeNo(l,j,i));
+      }
+    }
+    addCNF0va();
+  }
+
+  for(int i=0;i<k;i++){
+    for(int l=0;l<y;l++){
+      for(int j=0;j<x;j++){
+        for(int m=0;m<y;m++){
+          for(int n=0;n<x;n++){
+            if(j!=n || l!=m){
+              //std::cout<<getNodeNo(j,l,i)<<std::endl;
+              //std::cout<<getNodeNo(n,m,i)<<std::endl;
+              //std::cout<<std::endl;
+              addCNF2va(getNodeNo(j,l,i),getNodeNo(n,m,i));
+            }
+          }
+        }
+      }
+    }
+  }
+
+  for(int i=0;i<k;i++){
+    for(int j=0;j<k;j++){
+      if(adjacency[i][j]==1){
+        for(int l=0;l<y;l++){
+          for(int m=0;m<x;m++){
+             addCNF3va(getNodeNo(m,l,i));
+             if(m-1!=-1){
+               addCNF1va(getNodeNo(m-1,l,j));
+             }
+             if(m+1!=x){
+               addCNF1va(getNodeNo(m+1,l,j));
+             }
+             if(l-1!=-1){
+               addCNF1va(getNodeNo(m,l-1,j));
+             }
+             if(l+1!=y){
+               addCNF1va(getNodeNo(m,l+1,j));
+             }
+          addCNF0va();
+          }
+        }
+      }
+    }
+  }
+
+  for(auto e:cnf){
+    outputfile<<e<<std::endl;
+  }
+
+  outputfile.close();
+
+  execl("./glueminisat-simp","input.dimacs","output");
+
+}
+
+int main(){
+
+  int x=3;
+  int y=3;
+  int k=8;
+  int adjacency[200][200];
+  adjacency[1][2]=1;
+  adjacency[2][3]=1;
+
+  cnfencoder(x,y,k,adjacency);
+
+
+
+  return 0;
+}
